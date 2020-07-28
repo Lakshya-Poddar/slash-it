d1ecd952817f09f11cb3f36e3c4404ffbde0ccbb
Lakshya-Poddar <lakshyapoddar1604@gmail.com>
2020-07-28 19:34:19 +0530

    random url redirect to error page

--- server.js
+++ server.js
@@ -48,14 +48,12 @@ app.get("/:hash", (req, res) => {
       console.log(doc);
       return res.redirect(doc.longUrl);
     } else {
-      if (process.env.NODE_ENV === "production") {
-        app.use(express.static("client/build"));
-        app.get("*", (req, res) => {
+      app.use(express.static("client/build"));
+      app.get("*", (req, res) => {
           res.sendFile(
             path.resolve(__dirname, "client", "build", "index.html")
           );
         });
-      }
     }
   });
 });
